75fd9c622c9b80a164dc997011b64a2b80c85b32
Jeff Conn <jeff.conn121@gmail.com>
2022-04-25 18:03:52 -0400

    testing context with button

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import React, {useContext} from "react"
 import './App.css';
+import { MainContext } from "./contextProvider";
+
 
 function App() {
+  
+const {color} = useContext(MainContext)
+
+function showColor() {
+  console.log(color)
+}
+
   return (
-    <div className="App">Hello World
+    <div className="App">
+       Context test <br></br>
+      <button onClick={showColor}> show color </button>
     </div>
   );
 }
