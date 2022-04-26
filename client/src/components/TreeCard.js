be2721227f6c6618d4a5920ac8450763a4d68064
Jeff Conn <jeff.conn121@gmail.com>
2022-04-26 12:49:49 -0400

    addition of css in tree card

--- client/src/components/TreeCard.js
+++ client/src/components/TreeCard.js
@@ -2,15 +2,18 @@ import React from 'react'
 import "../styles/TreeCard.css"
 
 function TreeCard(props) {
-    const {name, type, color, size, price } = props
+    const {name, type, color, size, price, imgUrl, description } = props
     console.log(props)
   return (
     <div className='tree-card-container'>
-        <h3 className='tree-type'>{type}</h3>
+        <p className='tree-type'>{type}</p>
         <h1 className='tree-name'>{name}</h1>
-        <h3 className='tree-color'>{color}</h3>
-        <h3 className='tree-size'>{size}</h3>
-        <h3 className='tree-price'>{price}</h3>
+        <img src={imgUrl} alt="cherry tree" className='tree-image'/>
+        <p className='tree-description'>{description}
+          <h3 className='tree-color'>{`Color: ${color}`}</h3>
+          <h3 className='tree-size'>{`Size: ${size}`}</h3>
+          <h3 className='tree-price'>{`Price: $${price}`}</h3>
+        </p>
     </div>
   )
 }
