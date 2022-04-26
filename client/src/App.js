37613a2094646ba5ed2b3448ba8d387c211ed481
Jeff Conn <jeff.conn121@gmail.com>
2022-04-25 20:24:02 -0400

    Set up of Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
-import React, {useContext} from "react"
 import './App.css';
-import { MainContext } from "./contextProvider";
+import React from "react"
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+// import { MainContext } from "./contextProvider";
+import {BrowserRouter as Router, Routes,Route, NavLink} from "react-router-dom"
 
 
 function App() {
-  
-const {color} = useContext(MainContext)
-
-function showColor() {
-  console.log(color)
-}
 
   return (
-    <div className="App">
-       Context test <br></br>
-      <button onClick={showColor}> show color </button>
-    </div>
+    <Router>
+      <header className='Header'>
+        <nav className='NavBar'>
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/services">Services</NavLink>
+              <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/services" element={<Services />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+      <footer>
+        <p>This is the footer</p>
+      </footer>
+    </Router>
   );
 }
 
